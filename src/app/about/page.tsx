1cfef74564fe9fd422978f2016bd639ead6427bb
leejieun2023 <lge9380@naver.com>
2024-03-14 20:17:14 +0900

    Feat: report 페이지 작성

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,23 +3,23 @@ import Image from "next/image";
 import { companyInfoType } from "@/types/types";
 
 const AboutPage = async () => {
-  const response = await fetch("http://localhost:4000/companyInfo");
+  const response = await fetch("http://localhost:4001/companyInfo");
   const company: companyInfoType = await response.json();
 
   return (
     <main className="flex justify-center">
-      <div>
-        <p>회사 이름: {company.name}</p>
-        <p>소개: {company.description}</p>
-      </div>
-      <div>
+      <div className="flex justify-evenly items-center gap-44 mt-10 min-w-max">
         <Image
           src={company.image}
           alt="company image"
-          width={400}
-          height={600}
+          width={200}
+          height={300}
           className="-z-20"
         />
+      </div>
+      <div>
+        <p>회사 이름: {company.name}</p>
+        <p>소개: {company.description}</p>
       </div>
     </main>
   );
--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -1,17 +1,26 @@
 import React from "react";
+import { TodoType } from "@/types/types";
 
 const ReportPage = async () => {
-    const response = await fetch("http://localhost:3000/api/todos", {
+    const response = await fetch("http://localhost:4001/todos", {
         next: {
-            revalidate: 10
-        }
+            revalidate: 10,
+        },
     });
-    const { data } = await response.json();
+    const todolist = await response.json();
+
+    const totalTodo = todolist.length;
+    const working = todolist.filter((item: TodoType) => item.isDone === false).length;
+    const done = todolist.filter((item: TodoType) => item.isDone === true).length;
 
     return (
-        <div>
-            <div>ReportPage</div>
-        </div>
+        <main>
+            <div>
+                <h1>📌 현재까지 {totalTodo}개의 TodoList가 등록되었습니다 (｡•̀ᴗ-)✧</h1>
+                <p>현재까지 {working}개의 할 일이 존재합니다 (ò_óˇ)💪</p>
+                <p>완료된 TodoList는 {done}개 입니다 (๑•̑з•̑๑)👍</p>
+            </div>
+        </main>
     )
 }
 
